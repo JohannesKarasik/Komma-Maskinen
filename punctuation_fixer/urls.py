fd5c164bbdc73ed99892db2f46a8d17931ac18cb
JohannesKarasik <johanneskarasikweb@gmail.com>
2024-11-25 13:31:34 -0500

    Update urls.py

--- punctuation_fixer/urls.py
+++ punctuation_fixer/urls.py
@@ -1,8 +1,16 @@
 # punctuation_fixer/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticSitemap
+
+sitemaps = {
+    'static': StaticSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('fixer.urls')),  # Include the app-level URLs from the 'fixer' app
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+
 ]
