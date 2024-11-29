7a6a7673b379d1e79732d8983550e9a99b4d5811
JohannesKarasik <johanneskarasikweb@gmail.com>
2024-11-28 23:49:48 -0500

    Update urls.py

--- fixer/urls.py
+++ fixer/urls.py
@@ -5,4 +5,7 @@ from . import views  # Import views from the fixer app
 
 urlpatterns = [
     path('', views.index, name='index'),  # Home page route (text or file upload)
+    path('page-one/', views.page_one, name='page_one'),
+    path('page-two/', views.page_two, name='page_two'),
+
 ]
