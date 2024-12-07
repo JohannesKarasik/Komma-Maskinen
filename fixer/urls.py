07e73428867861645a728188d3083f037593c24c
JohannesKarasik <johanneskarasikweb@gmail.com>
2024-12-07 13:00:23 -0500

    Update urls.py

--- fixer/urls.py
+++ fixer/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),  # Home page route (text or file upload)
     path('stavekontrol/', views.stavekontrol, name='stavekontrol'),  # Add this line
     path('home/', views.home, name='home'),  # Corrected line
+    path('policy/', views.policy, name='policy'),
 
 
 ]
